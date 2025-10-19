3ce569b84c20f29b03c621974d64c50e66769f71
Ilya Rozhkov <imrozhkov@gmail.com>
2025-10-19 13:00:15 +0300

    Fixed disko.nix

--- hosts/shellbook/disko.nix
+++ hosts/shellbook/disko.nix
@@ -2,14 +2,14 @@
   disko.devices = {
     disk.main = {
       type = "disk";
-      device = "/dev/nvme0n1";
+      device = "/dev/nvme0n1";            # проверьте ваш диск
       content = {
         type = "gpt";
         partitions = {
           ESP = {
             name = "ESP";
             size = "1G";
-            type = "ef00";
+            type = "ef00";                 # EFI System Partition
             content = {
               type = "filesystem";
               format = "vfat";
@@ -21,11 +21,11 @@
           crypt = {
             name = "crypt";
             size = "100%";
-            type = "8309";
+            type = "8309";                 # Linux LUKS (можно и 8300, но 8309 точнее)
             content = {
               type = "luks";
               name = "cryptroot";
-              settings = { allowDiscards = true; };
+              settings = { allowDiscards = true; }; # TRIM для NVMe
               content = {
                 type = "lvm_pv";
                 vg = "vg0";
@@ -40,18 +40,16 @@
       type = "lvm_vg";
       lvs = {
         swap = {
-          type = "lvm_lv";
-          size = "22G"; 
+          size = "22G";                    # для гибернации (16 ГБ RAM + запас)
           content = { type = "swap"; resumeDevice = true; };
         };
 
         root = {
-          type = "lvm_lv";
           size = "100%FREE";
           content = {
             type = "btrfs";
             extraArgs = [ "-L" "nixos" ];
-
+            # базовые опции (унаследуются сабвольюмами, если не переопределены)
             mountOptions = [ "compress=zstd:3" "noatime" "ssd" "space_cache=v2" ];
             subvolumes = {
               "@".mountpoint = "/";
@@ -64,27 +62,22 @@
                 mountpoint = "/var/log";
                 mountOptions = [ "compress=zstd:3" "noatime" ];
               };
-
               "@cache" = {
                 mountpoint = "/var/cache";
                 mountOptions = [ "compress=no" "noatime" ];
               };
-
               "@docker" = {
                 mountpoint = "/var/lib/docker";
                 mountOptions = [ "compress=no" "noatime" ];
               };
-
               "@containers" = {
                 mountpoint = "/var/lib/containers";
                 mountOptions = [ "compress=no" "noatime" ];
               };
-
               "@tmp" = {
                 mountpoint = "/tmp";
                 mountOptions = [ "noatime" ];
               };
-
               "@steamlib" = {
                 mountpoint = "/var/games/steam";
                 mountOptions = [ "compress=no" "noatime" ];
