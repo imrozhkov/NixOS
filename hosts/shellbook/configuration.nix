13f156efaf3b7dae3b0cd9d61656f309609ad106
Ilya Rozhkov <imrozhkov@gmail.com>
2025-10-21 21:53:46 +0300

    Modified configuration.nix

--- hosts/shellbook/configuration.nix
+++ hosts/shellbook/configuration.nix
@@ -1,72 +1,78 @@
 { config, pkgs, lib, ... }:
 
 {
-  ########################
-  # Базовая системная инфа
-  ########################
+  ############################################
+  # Host / locale / time
+  ############################################
   networking.hostName = "shellbook";
   time.timeZone = "Europe/Moscow";
+
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
-    "ru_RU.UTF-8/UTF-8";
+    "ru_RU.UTF-8/UTF-8"
   ];
 
-  ########################
-  # Загрузчик (UEFI)
-  ########################
+  # Шрифты с поддержкой кириллицы и emoji
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    nerd-fonts.jetbrains-mono 
+  ];
+
+  fonts.fontconfig.defaultFonts.monospace = [
+    "JetBrainsMono Nerd Font"
+  ];
+
+  ############################################
+  # Bootloader + LUKS
+  ############################################
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  ########################
-  # LUKS (root внутри LUKS2→LVM)
-  ########################
-  # В disko раздел назван "crypt"; это удобно использовать по label:
+  # LUKS-том, созданный disko (имя из partlabel)
   boot.initrd.luks.devices.cryptroot = {
-    device = "/dev/disk/by-partlabel/crypt";
-    allowDiscards = true;   # TRIM для NVMe
+    device = "/dev/disk/by-partlabel/disk-main-crypt";
+    allowDiscards = true;
   };
 
-  ##########################################
-  # Подкачка: swap-LV (для гибернации) + zram
-  ##########################################
-  # swap-LV создан disko как /dev/mapper/vg0-swap
-  swapDevices = [ { device = "/dev/mapper/vg0-swap"; } ];
-  # куда писать образ гибернации
-  boot.resumeDevice = "/dev/mapper/vg0-swap";
-
-  # Лёгкая подкачка в RAM (не для гибернации)
-  zramSwap = {
-    enable = true;
-    memoryPercent = 75;  # ~12 ГБ на 16 ГБ RAM
-  };
-
-  # Режим sleep→auto-hibernate (как в macOS)
+  ############################################
+  # Power: suspend-then-hibernate
+  ############################################
+  services.logind.lidSwitch = "suspend-then-hibernate";
+  services.logind.lidSwitchDocked = "ignore";
   systemd.sleep.extraConfig = ''
     HibernateDelaySec=30min
   '';
 
-
-  programs.zsh.enable = true;
-  system.stateVersion = "25.05";
-
-  ########################
-  # Wayland / Hyprland
-  ########################
-  services.xserver.enable = false;   # без X11
-  programs.hyprland = {
+  ############################################
+  # ZRAM + nix flakes
+  ############################################
+  zramSwap = {
     enable = true;
-    xwayland.enable = true;          # полезно для несовместимых приложений
+    memoryPercent = 75;
   };
 
-  # Порталы (скриншот/шаринг экрана в Wayland)
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  ############################################
+  # Wayland / Hyprland
+  ############################################
+  services.xserver.enable = false;
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
+    xdg-desktop-portal-gtk
   ];
 
-  # Аудио/видео стек (PipeWire)
+  ############################################
+  # Audio/Video stack
+  ############################################
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -75,47 +81,77 @@
   };
   security.rtkit.enable = true;
 
-  ########################
-  # Сеть, обновления прошивок
-  ########################
-  networking.networkmanager.enable = true;
+  ############################################
+  # Firmware, graphics, updates
+  ############################################
+  # Wi-Fi: iwlwifi + iwd (через NetworkManager)
+  hardware.enableRedistributableFirmware = true;
+  hardware.firmware = [ pkgs.linux-firmware ];
+
+  boot.kernelModules = [ "iwlwifi" ];
+
+  # iwd как backend для NetworkManager (вместо wpa_supplicant)
+  services.iwd.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+  };
+
   networking.firewall.enable = true;
   services.fwupd.enable = true;
 
-  ########################
-  # Микрокод Intel, графика
-  ########################
   hardware.cpu.intel.updateMicrocode = true;
   hardware.opengl.enable = true;
 
-  ########################
-  # Пользователь/шелл
-  ########################
-  users.users.imrozhkov = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "video" "audio" "networkmanager" ];
-    shell = pkgs.zsh;
-    # чтобы зайти после установки, зададим временный пароль:
-    initialPassword = "changeme"; # смените: `passwd imrozhkov`
+  ############################################
+  # SSH: allow from LAN only
+  ############################################
+  services.openssh = {
+    enable = true;
+    openFirewall = false; # не открываем всем
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
   };
+
+  # Разрешаем 22/tcp только из своей подсети
+  networking.firewall.extraInputRules = ''
+    ip  saddr 192.168.1.0/24 tcp dport 22 accept
+    ip6 saddr fe80::/10      tcp dport 22 accept
+  '';
+
+  ############################################
+  # Users / shell
+  ############################################
+  programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
-  ########################
-  # Пакеты по-минимуму
-  ########################
+  users.users.imrozhkov = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio" "networkmanager" "docker" ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJd4G7+N8wOlLdpI44TrtgQ4bl8o+oVL5/4YWbw1PxEo imrozhkov@wsl"
+    ];
+  };
+
+  ############################################
+  # Packages
+  ############################################
   environment.systemPackages = with pkgs; [
-    git wget curl neovim vim htop ripgrep fd tree
+    git wget curl neovim htop tree
     btrfs-progs lvm2 cryptsetup
-    wl-clipboard
+    firefox
+    kitty
     obs-studio
   ];
 
-  ########################
-  # Flakes в системе
-  ########################
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Firefox под Wayland (Hyprland)
+  environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
 
-  ########################
-  # (hardware-configuration.nix будет сгенерирован установщиком)
-  ########################
+  ############################################
+  # Misc
+  ############################################
+  system.stateVersion = "25.05";  # не менять при обновлениях системы
 }
