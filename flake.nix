4bb626b8c4c46c022b9a459ed8db3631beba87f9
Ilya Rozhkov <imrozhkov@gmail.com>
2025-10-26 11:41:09 +0300

    feat(flake): define pkgsUnstable and pass to HM

--- flake.nix
+++ flake.nix
@@ -1,39 +1,36 @@
 {
-    description  = "NixOS, disko, HM, BTRFS, LUKS2, Hyprland)";
+  description = "NixOS, disko, HM, BTRFS, LUKS2, Hyprland)";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        disko.url = "github:nix-community/disko";
-        
-        home-manager = {
-            url = "github:nix-community/home-manager/release-25.05";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    disko.url = "github:nix-community/disko";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { self, nixpkgs, nixpkgs-unstable, disko, home-manager, ... }:
-        let
-            system = "x86_64-linux";
-            pkgsUnstable = import nixpkgs-unstable { inherit system; }
-        in {
-            nixosConfigurations.shellbook = nixpkgs.lib.nixosSystem {
-                inherit system;
+  outputs = { self, nixpkgs, nixpkgs-unstable, disko, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgsUnstable = import nixpkgs-unstable { inherit system; };
+    in {
+      nixosConfigurations.shellbook = nixpkgs.lib.nixosSystem {
+        inherit system;
 
-                modules = [
-                    disko.nixosModules.disko
-                    ./hosts/shellbook/disko.nix
-                    ./hosts/shellbook/configuration.nix
+        modules = [
+          disko.nixosModules.disko
+          ./hosts/shellbook/disko.nix
+          ./hosts/shellbook/configuration.nix
 
-                    home-manager.nixosModules.home-manager {
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.extraSpecialArgs = { inherit pkgsUnstable; };
-                        home-manager.users.imrozhkov = import ./home/imrozhkov/home.nix;
-                    }
-                ];
-                
-                
-            };
-        };
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit pkgsUnstable; };
+            home-manager.users.imrozhkov = import ./home/imrozhkov/home.nix;
+          }
+        ];
+      };
+    };
 }
