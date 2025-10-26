950b8edd8ff83ac78872bb50e62dec9fce5d2dd6
Ilya Rozhkov <imrozhkov@gmail.com>
2025-10-26 11:42:12 +0300

    feat(home): use rofi from unstable and manage authorized_keys via HM

--- home/imrozhkov/home.nix
+++ home/imrozhkov/home.nix
@@ -21,4 +21,14 @@
   ] ++ [
     pkgsUnstable.rofi
   ];
+
+  home.file.".ssh".directory = true;
+  home.file.".ssh".mode = "0700";
+
+  home.file.".ssh/authorized_keys" = {
+    text = ''
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJd4G7+N8wOlLdpI44TrtgQ4bl8o+oVL5/4YWbw1PxEo imrozhkov@wsl
+'';
+    mode = "0600";
+  };
 }
